b5ca3dcfb860a428647f9200cca16824bb64b254
Rene Schwartz <rene.j.schwartz@gmail.com>
2025-09-05 23:45:38 +0300

    feat: main menu interactivity

--- godot_project/source/main_menu_screen.gd
+++ godot_project/source/main_menu_screen.gd
@@ -3,7 +3,11 @@ extends Node
 @export var first_selected_element: Control
 @export var main_menu_container: Control
 
-func _on_time_to_select_first_element() -> void:
+func _ready():
+	select_first_element()
+	
+
+func select_first_element() -> void:
 	first_selected_element.grab_focus()
 
 func _on_b_quit_game_pressed() -> void:
